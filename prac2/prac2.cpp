0a8ec005fdbaa9f089d4717a041d7aa777d18007
Jett-Mercer <merjr001@mymail.unisa.edu.au>
2024-03-05 10:09:00 +1030

    test commit

---
+++ prac2/prac2.cpp
@@ -0,0 +1,16 @@
+﻿// prac2.cpp : Defines the entry point for the application.
+//
+
+#include "prac2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+
+
+	
+
+}
